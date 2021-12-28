df0678c389f782c0959ad0496e442a6fb2ca48e8
Fabi Tureo <fabian.tureo@ibisdev.tech>
2021-12-28 11:34:36 -0300

    Cleaning unused dependency in index

--- Routes/index.js
+++ Routes/index.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const userController = require("../Controllers/userController");
 const validator = require("../Controllers/validator");
 
-const passport = require("passport");
-require("../Config/passport");
-
 router.route("/user/sign-in").post(userController.signIn);
 
 router
